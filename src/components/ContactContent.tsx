b6eff648a3979997a1baea61fd2ebc373d36492b
avneesh atri <atri.avneesh@gmail.com>
2024-10-17 20:54:44 +0530

    Updated google map link

--- src/components/ContactContent.tsx
+++ src/components/ContactContent.tsx
@@ -19,7 +19,7 @@ const ContactContent = () => {
                     <div className="row">
                         <div className="col-lg-8 col-12">
                             <div className="contact-page-map">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d503103.25504622894!2d-118.94592338887756!3d34.065964560335836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c75ddc27da13%3A0xe22fdf6f254608f4!2z4Kay4Ka4IOCmj-CmnuCnjeCmnOCnh-CmsuCnh-CmuCwg4KaV4KeN4Kav4Ka-4Kay4Ka_4Kar4KeL4Kaw4KeN4Kao4Ka_4Kav4Ka84Ka-LCDgpq7gpr7gprDgp43gppXgpr_gpqgg4Kav4KeB4KaV4KeN4Kak4Kaw4Ka-4Ka34KeN4Kaf4KeN4Kaw!5e0!3m2!1sbn!2sbd!4v1497798250780" width="100%" height="350" frameBorder="0" allowFullScreen></iframe>
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1367.3980101597674!2d78.2169686!3d29.3598483!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390be958e0c8d12d%3A0xbc6189fb9ec5efe9!2sKarondi%2C%20Uttar%20Pradesh%20246728!5e1!3m2!1sen!2sin!4v1729178509249!5m2!1sen!2sin" width="100%" height="350" frameBorder="0" allowFullScreen></iframe>
                             </div>
                         </div>
                         <div className="col-lg-4 col-12">
